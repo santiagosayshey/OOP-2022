c6caa1c6d5a65526b40683510f30db6cd72494e7
santiagosayshey <chaucted@gmail.com>
2022-07-25 07:02:02 +0930

    main now gets 2 inputs and sums them together

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 
 int main() {
 
-    std::cout << "Hello World!" << std::endl;
+    // good practice to intialise variables as soon as you declare them
+    int a=0 , b=0 , c=0;
 
+    std::cout << "Enter a:\n";
+    std::cin >> a;
+
+    std::cout << "Enter b:\n";
+    std::cin >> b;
+
+    c = a + b;
+
+    std::cout <<"a+b = " << c << std::endl;
 
     return 0;
 
