3ce661caf53fd338247cc81a398f9364a301ea4e
santiagosayshey <chaucted@gmail.com>
2022-07-25 05:45:47 +0930

    changed main  to include original test cases

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 extern int count(int[], int);
 
 int main() {
-    int array[8] = {4,5,6,7,8,1,3,24};
-    std::cout << "The number is: " << count(array, 8) << std::endl;
+    int array[5] = {4,5,6,7,8};
+    std::cout << "The number is: " << count(array, 5) << std::endl;
     return 0;
 }
