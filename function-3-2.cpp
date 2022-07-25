9683fb7f705ce74254da202f4acd066eba678f16
santiagosayshey <chaucted@gmail.com>
2022-07-25 21:20:40 +0930

    q3-2 done

---
+++ function-3-2.cpp
@@ -0,0 +1,24 @@
+// determines the median of a sorted array
+#include <iostream>
+#include <algorithm>
+
+
+int median_array(int array[], int n) {
+    
+	for(int i=0;i<n;i++)
+	{		
+		for(int j=i+1;j<n;j++)
+		{
+			if(array[i]>array[j])
+			{
+				int temp =array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
+
+    int median = array[n/2];
+    return median;
+
+}
