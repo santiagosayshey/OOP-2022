ecf2da08fe953927399e75db54ac4f3f7077f1dc
santiagosayshey <chaucted@gmail.com>
2022-07-25 22:17:42 +0930

    adding more bounds

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -4,7 +4,22 @@
 
 int median_array(int array[], int n) {
 
-
+	if (n%2 == 0 || n < 1) {
+		return 0;
+	}
+	
+	for(int i=0;i<n;i++)
+	{		
+		for(int j=i+1;j<n;j++)
+		{
+			if(array[i]>array[j])
+			{
+				int temp =array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
 
     int median = array[n/2];
     return median;
