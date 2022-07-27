62b5e232950cc444a61b0c0d9cf4a6544120ccc3
santiagosayshey <chaucted@gmail.com>
2022-07-27 11:50:24 +0930

    fixed

--- Lectures/Week1/prac1/main-1-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern double array_mean(int array[], int n);
-
-int main() {
-    int array[5]={1,1,1,1,3};
-    std::cout << "The average is: " << array_mean(array, 5) << std::endl;
-    return 0;
-}
