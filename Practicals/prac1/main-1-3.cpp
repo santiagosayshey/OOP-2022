a0634484e80c3a9b3d04ae654a9e6d5ce0ab50c5
santiagosayshey <chaucted@gmail.com>
2022-07-27 12:20:40 +0930

    template

--- Practicals/prac1/main-1-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern int num_count(int array[], int n, int number);
-
-int main() {
-    int array[9] = {4,5,6,7,11,6,8,7,5};
-    std::cout << "Numbers in the array = 5: " << num_count(array, 9, 5) << std::endl;
-    return 0;
-}
--- Practicals/prac1/main-2-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern void two_five_nine(int array[], int n);
-
-int main(void) {
-    int array[10]={1,2,2,2,3,4,5,5,7,9};
-    two_five_nine(array, 10);
-    return 0;
-}
