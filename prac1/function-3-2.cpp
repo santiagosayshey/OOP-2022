c55ae904c58da6f97387dac3c73e58d178aaff78
santiagosayshey <chaucted@gmail.com>
2022-07-27 11:21:36 +0930

    formatted nicely for main

--- prac1/function-3-2.cpp
+++ prac1/function-3-2.cpp
@@ -8,6 +8,19 @@ int median_array(int array[], int n) {
 		return 0;
 	}
 
+	for(int i=0;i<n;i++)
+		{		
+			for(int j=i+1;j<n;j++)
+			{
+				if(array[i]>array[j])
+				{
+					int temp =array[i];
+					array[i]=array[j];
+					array[j]=temp;
+				}
+			}
+		}
+		
     int median = array[n/2];
     return median;
 
