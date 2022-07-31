d41e4d4f13dc01883f86f6c526ba1f9645c3f7ae
santiagosayshey <chaucted@gmail.com>
2022-08-01 05:41:20 +0930

    added end of line character to final output

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,10 +5,11 @@ using namespace std;
 void print_scaled(int arr[3][3],int scale) {
         for (int i = 0; i<3; i++) {
             for (int j = 0; j<3; j++) {
-                cout << scale*arr[i][j];
+                cout << scale*arr[i][j] << " ";
             }
             cout << endl;
         }
+        cout << endl;
                 
 
 }
--- function-1-5.cpp
+++ function-1-5.cpp
@@ -7,8 +7,9 @@ void print_summed(int array1[3][3],int array2[3][3]) {
 
     for (int i = 0; i<3; i++) {
         for (int j = 0; j<3; j++) {
-            cout << array1[i][j] + array2[i][j];
+            cout << array1[i][j] + array2[i][j] << " ";
         }
         cout << endl;
     }
+    cout << endl;
 }
