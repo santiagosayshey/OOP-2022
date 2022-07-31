ba5b74dbedb8106f5565a6c257ec903cfbb73ea7
santiagosayshey <chaucted@gmail.com>
2022-08-01 05:25:10 +0930

    fixed 1-4. Output is multiplied by scale instead of fixed 3.

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -8,7 +8,7 @@ int main() {
 
 
     int scale = 3 ;
-    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
+    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,4}} ;
     print_scaled(threebythree,scale) ;
 
     return 0;
