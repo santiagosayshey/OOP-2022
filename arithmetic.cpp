e23fc91e6ab9bab95361a103fc167d3cfdbf1b15
santiagosayshey <chaucted@gmail.com>
2022-08-01 07:36:10 +0930

    finished binary arithmetic

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -4,21 +4,34 @@ using namespace std;
 
 int main() {
 
-    int quotient = 56;
-    int divisor=2;
-    int binaryNum[32];
-    int i=0;
+    int count1=1;
+    int decimal1 = 0;
+    int binary_digits[6]={1,1,1,0,0,0};
+    int number_of_digits=6;
 
-    while (quotient != 0) {
-        binaryNum[i] = quotient % divisor;
-        i++;
-        quotient /= divisor;
+    for (int i = number_of_digits-1; i >= 0; i--) {
+        if (binary_digits[i]==1) {
+            decimal1 += count1;
+        }
+        count1 *= 2;
     }
+    cout << decimal1 << endl;
 
-    for (int j = i - 1; j >= 0; j--) {
-        cout << binaryNum[j];
+    int count2=1;
+    int decimal2 = 0;
+    int binary_digits2[9]={1,0,1,1,1,0,1,1};
+    int number_of_digits2=9;
+
+    for (int i = number_of_digits2-1; i >= 0; i--) {
+        if (binary_digits2[i]==1) {
+            decimal2 += count2;
+        }
+        count2 *= 2;
     }
-    cout << endl;
+    cout << decimal2 << endl;
+
+    int trueDecimal = decimal1+decimal2;
+    cout << trueDecimal << endl;
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 mymake: transform.cpp
 		g++ -o transform transform.cpp
+		g++ -o arithmetic arithmetic.cpp
