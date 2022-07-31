5a0f5dd78ae255e3b713a046d01d7f8a949fc8bc
santiagosayshey <chaucted@gmail.com>
2022-08-01 05:28:59 +0930

    fixed output message

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void print_scaled(int arr[3][3],int scale) {
         for (int i = 0; i<3; i++) {
             for (int j = 0; j<3; j++) {
-                cout << scale*arr[i][j] <<;
+                cout << scale*arr[i][j];
             }
             cout << endl;
         }
