53bca3c80e94d6060cb933651c25ca19417861ef
santiagosayshey <chaucted@gmail.com>
2022-08-01 07:24:49 +0930

    new

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -6,22 +6,17 @@ int main() {
 
     int quotient = 56;
     int divisor=2;
-    int temp[32];
-    int binaryArr[32];
+    int binaryNum[32];
     int i=0;
-    int h=0;
 
     while (quotient != 0) {
-        temp[i] = quotient % divisor;
+        binaryNum[i] = quotient % divisor;
         i++;
         quotient /= divisor;
     }
 
     for (int j = i - 1; j >= 0; j--) {
-        binaryArr[h]=temp[j];
-        cout << binaryArr[h];
-        h++;
-
+        cout << binaryNum[j];
     }
     cout << endl;
 
