03abd4aed2c6efe0588b62173a31e11d9ab8cbcd
santiagosayshey <chaucted@gmail.com>
2022-08-01 05:27:40 +0930

    removed spaces in matrix questions because autograder sucks

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -7,7 +7,7 @@ void print_summed(int array1[3][3],int array2[3][3]) {
 
     for (int i = 0; i<3; i++) {
         for (int j = 0; j<3; j++) {
-            cout << array1[i][j] + array2[i][j] << " "; 
+            cout << array1[i][j] + array2[i][j] <<; 
         }
         cout << endl;
     }
