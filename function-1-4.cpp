ed390376f826de7cdaa0e78264553363e85e4acc
santiagosayshey <chaucted@gmail.com>
2022-08-01 13:26:10 +0930

    fixed spaces v3

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -6,13 +6,11 @@ void print_scaled(int arr[3][3],int scale) {
         for (int i = 0; i<3; i++) {
             for (int j = 0; j<3; j++) {
                 cout << scale*arr[i][j];
-                if (j!= 2) {
+                if (j<2) {
                     cout << " ";
                 }
             }
             cout << endl;
         }
-        cout << endl;
-                
 
 }
--- function-1-5.cpp
+++ function-1-5.cpp
@@ -8,9 +8,9 @@ void print_summed(int array1[3][3],int array2[3][3]) {
     for (int i = 0; i<3; i++) {
         for (int j = 0; j<3; j++) {
             cout << array1[i][j] + array2[i][j];
-            if (j!= 2) {
-                    cout << " ";
-                }
+            if (j<2) {
+                cout << " ";
+            }
         }
         cout << endl;
     }
