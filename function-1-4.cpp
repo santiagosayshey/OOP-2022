25a9af9d5bc22ee0549463e05b5a047a47f1201b
santiagosayshey <chaucted@gmail.com>
2022-08-01 13:18:59 +0930

    added if statement to add spaces

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,7 +5,10 @@ using namespace std;
 void print_scaled(int arr[3][3],int scale) {
         for (int i = 0; i<3; i++) {
             for (int j = 0; j<3; j++) {
-                cout << scale*arr[i][j] << " ";
+                cout << scale*arr[i][j];
+                if (j!= 2) {
+                    cout << " ";
+                }
             }
             cout << endl;
         }
