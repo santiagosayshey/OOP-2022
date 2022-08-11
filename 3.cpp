fd53fc999807c83082fee137237da92543914307
santiagosayshey <chaucted@gmail.com>
2022-08-12 08:15:53 +0930

    fixed

--- 3.cpp
+++ 3.cpp
@@ -38,11 +38,7 @@ int main() {
     printer(array);
     string courses[] = {"Maths","History","Science","Geography"};
     string students[] = {"Sam","Dan","Blam","Can"};
-    int report_card[][4] = {{1,2,3,4},
-                            {5,6,7,8},
-                            {1,3,5,7},
-                            {2,4,6,8}};
-    int report_card
+    int report_card[4][4] = {{1,2,3,4},{5,6,7,8},{1,3,5,7},{2,4,6,8}};
     int nstudents = 4;
     print_class(courses,students,report_card,nstudents);
 
