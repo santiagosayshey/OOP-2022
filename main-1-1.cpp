2741e22c18f6ccaea0ffcc4b266e83288f75f861
santiagosayshey <chaucted@gmail.com>
2022-08-13 18:38:29 +0930

    1-1 done

---
+++ main-1-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
+
+int main() {
+
+    int n=3;
+    string array[3][2]={{"Sam","Dan"},
+                        {"Clam","Ham"},
+                        {"Bam","Nam"}};
+
+    string newArray[3][2];
+
+    string_2d_copy(array,newArray,n);
+
+    return 0;
+}
