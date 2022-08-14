552eafdac4c000c84028a124849b511740dd5a46
santiagosayshey <chaucted@gmail.com>
2022-08-15 00:11:28 +0930

    fixed for gradescope

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -9,7 +9,6 @@ void copy_integers(int old_array[],int new_array[],int length) {
 
     for (int i = 0; i<length; i++) {
         *(ptr2+i)=*(ptr+i);
-        cout << *(ptr+i) << endl;
     }
 
 
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -9,7 +9,6 @@ void copy_doubles(double *old_array,double *new_array,int length) {
 
     for (int i = 0; i<length; i++) {
         *(ptr2+i)=*(ptr+i);
-        cout << *(ptr+i) << endl;
     }
 
 
