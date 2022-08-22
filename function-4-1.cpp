d816653bc9af442c62eb250ccc400de770e186c5
santiagosayshey <chaucted@gmail.com>
2022-08-22 16:12:31 +0930

    a

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int secondSmallestSum(int *numbers,int length) {
 
     int sum;
+    // use gauss' formula to find the sum of all positive numbers before n
+    // this gives the size of the array of sums
+
     int size = (length*length+length)/2;
     int arr[size];
     int a =0;
 
+    // get all sums
     for (int i = 0; i< length; i++) {
         sum = 0;
         for (int j = i; j < length; j++) {
@@ -18,6 +22,8 @@ int secondSmallestSum(int *numbers,int length) {
         }
     }
 
+
+    // quick sort array
     for(int i=0;i<size;i++) {		
 		for(int j=i+1;j<size;j++) {
 			if(arr[i]>arr[j]) {
@@ -28,6 +34,7 @@ int secondSmallestSum(int *numbers,int length) {
 		}
 	}
 
+    // return second smallest
     return arr[1];
 }
 
