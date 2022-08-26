9178dcfb22285eebb119d0067836ed81067a39e7
santiagosayshey <chaucted@gmail.com>
2022-08-26 14:32:47 +0930

    done

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
-int main() {
+bool is_more_positive(int vals[], int length) {
 
+    int pos=0;
+    int neg=0;
+    for (int i =0; i<length; i++) {
+        if (vals[i]<0) {
+            neg++;
+        }
+        else if (vals[i]>0) {
+            pos++;
+        }
+    }
+    if (pos>neg) {
+        return true;
+    }
+    else {
+        return false;
+    }
     return 0;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
+extern bool is_more_positive(int vals[], int length);
+
 int main() {
+
+    int vals[] = {1,2,-1,-2,-5};
+
+    bool pos = is_more_positive(vals,5);
+
+    if (pos==true) {
+        cout << "There are more postive numbers than negative numbers" << endl;
+    }
+    else {
+        cout << "There are more negative numbers than postive numbers" << endl;
+    }
 
     return 0;
 }
