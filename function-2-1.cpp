279b83ec0b470344fd705b675ecd2c26672500a6
santiagosayshey <chaucted@gmail.com>
2022-08-26 14:27:00 +0930

    a

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
-int main() {
+string get_temp_phase(int temp) {
 
+    if (temp < 274) {
+        return "ice";
+    }
+    else if (temp > 273 && temp < 374) {
+        return "liquid";
+    }
+    else if (temp > 373 && temp < 4701) {
+        return "gas";
+    }
+    else {
+        return "decomposed";
+    }
     return 0;
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
+extern string get_temp_phase(int temp);
+
 int main() {
+
+    int temp=273;
+
+    cout << "phase at " << temp << "K is: " << get_temp_phase(temp) << endl;
 
     return 0;
 }
