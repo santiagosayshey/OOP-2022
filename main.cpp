1a907992294498100748c47f10b42b8280fee0f8
santiagosayshey <chaucted@gmail.com>
2022-08-29 18:03:13 +0930

    a

--- main.cpp
+++ main.cpp
@@ -3,55 +3,56 @@
 
 using namespace std;
 
-class publication {
+class A {
+
     protected:
-        string title;
-        string author;
-        int year;
+        int _s;
+
     public:
-        publication(string title, string author, int year):
-        title(title), author(author), year(year) { }
-        publication(): publication("No Title", "No Author", 0) {}
-        void print() {
-            cout << "Title: " << this->title << endl;
-            cout << "Author: " << this->author << endl;
-            cout << "Year: " << this->year << endl;
+        A(int s): _s(s){}
+        A():A(-1) {}
+
+        // runtime polymorphism
+         void print() {
+            cout << "S = " << this->_s << endl;
         }
 
 };
 
-class book: public publication {
+class B : public A {
+
     private:
-        string publisher;
-        int num_pages;
-        int ISBN;
+        double _k;
+    
     public:
-        book(string title, string author, int year, string publisher, int pages, int ISBN) : publication(title,author,year), num_pages(pages), ISBN(ISBN), publisher(publisher) {}
-        book(): book("No Title", "No Author", 0, "",0,0 ) {}
-        // we can 'modify' existing functions
+        B(int s,double k):A(s), _k(k) {}
+        B():B(0,0) {}
+
+        // overiding the function
         void print() {
-            publication::print();
-            cout << "publisher: " << this->publisher << endl;
-            cout << "num_pages: " << this->num_pages << endl;
-            cout << "ISBN: " << this->ISBN << endl;
+            cout << "S = " << this->_s << " K = "<< this->_k << endl;
         }
-        // or we can overload them by changing the parameters (called changing the signature)
-        void print(bool short_print) {
-            if (short_print) {
-                publication::print();
-            }
-            else {
-            publication::print();
-            cout << "publisher: " << this->publisher << endl;
-            cout << "num_pages: " << this->num_pages << endl;
-            cout << "ISBN: " << this->ISBN << endl;
-            }
-        }
+
 };
+
+void test(A* a) {
+    a->print();
+}
 
 int main() {
+/*     
+    A* a1;
+    B b1(500,0.123);
+    // a pointer to a base class can point to an object from a derived class
+    a1 = &b1;
+    // print is statically binded to the parent class
+    a1->print(); */
 
-    book b("OOP", "Sam", 2022, "Adl UNI", 500, 12332);
-    b.print(true);
+    A a1(564);
+    B b1(500,0.123);
+
+    test(&a1);
+    test(&b1);
+
     return 0;
 }
