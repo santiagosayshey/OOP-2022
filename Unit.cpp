8108001313a3295abde0a459eb7e78bf097fc837
santiagosayshey <chaucted@gmail.com>
2022-09-09 16:41:48 +0930

    aa

--- Unit.cpp
+++ Unit.cpp
@@ -8,9 +8,26 @@ Unit::Unit()
 } 
 Unit::Unit(int unit_val, int num_beds, double unit_size)
 {
-    unitValue=unit_val;
-    numBeds=num_beds;
-    unitSize=unit_size;
+    if (unit_val>0) {
+        unitValue=unit_val;
+    }
+    else {
+        unitValue=0;
+    }
+
+    if (num_beds>0) {
+        numBeds=num_beds;
+    }
+    else {
+        numBeds=0;
+    }
+
+    if (unit_size>0) {
+        unitSize=unit_size;
+    }
+    else {
+        unitSize=0;
+    }
 }
 
 int Unit::get_num_bedrooms()
